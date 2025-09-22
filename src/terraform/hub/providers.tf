e11b803895251d463264207562c3f0e764bed23f
emolina5 <emolinam5@gmail.com>
2025-09-22 21:12:15 +0200

    change: key in hub backend

--- src/terraform/hub/providers.tf
+++ src/terraform/hub/providers.tf
@@ -11,7 +11,7 @@ terraform {
     resource_group_name  = "rg-tfland-d-01"
     storage_account_name = "sttflandd01"          # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tflandd"               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "dev.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.\
+    key                  = "hub.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.\
   }
 }
 
