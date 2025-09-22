d236bee54fc94b28422a0c32cfd08631e0cff42e
emolina5 <emolinam5@gmail.com>
2025-09-22 21:35:15 +0200

    fix: string instead of required list

--- src/terraform/hub/main.tf
+++ src/terraform/hub/main.tf
@@ -8,12 +8,13 @@ resource "azurerm_virtual_network" "hub" {
   location            = azurerm_resource_group.hub.location
   resource_group_name = azurerm_resource_group.hub.name
   address_space       = ["10.0.0.0/16"]
+
 }
 
 resource "azurerm_subnet" "this" {
   for_each = {
-    gw = "10.0.0.0/24"
-    fw = "10.0.1.0/24"
+    gw = ["10.0.1.0/24"],
+    fw = ["10.0.2.0/24"]
   }
   name                 = "snet-${each.key}-dev-01"
   resource_group_name  = azurerm_resource_group.hub.name
