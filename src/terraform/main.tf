79af493a107ca48ca94dbb6654116666e5d5c3b2
emolina5 <emolinam5@gmail.com>
2025-10-01 09:08:33 +0200

    feat: parametrization

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,7 +1,7 @@
 #### HUB ####
 locals {
+  location = "North Europe"
   map = zipmap(["gw", "fw"], cidrsubnets("10.0.0.0/16", 8, 8))
-
   spokes = {
     spoke1 = {
       vnet = "10.1.0.0/16"
@@ -14,12 +14,12 @@ locals {
 }
 
 resource "azurerm_resource_group" "hub" {
-  name     = "rg-hub-dev-01"
-  location = "North Europe"
+  name     = "rg-hub-${var.environment}-01"
+  location = local.location
 }
 
 resource "azurerm_virtual_network" "hub" {
-  name                = "vnet-hub-dev-01"
+  name                = "vnet-hub-${var.environment}-01"
   location            = azurerm_resource_group.hub.location
   resource_group_name = azurerm_resource_group.hub.name
   address_space       = ["10.0.0.0/16"]
@@ -28,7 +28,7 @@ resource "azurerm_virtual_network" "hub" {
 
 resource "azurerm_subnet" "hub" {
   for_each             = local.map
-  name                 = "snet-${each.key}-dev-01"
+  name                 = "snet-${each.key}-${var.environment}-01"
   resource_group_name  = azurerm_resource_group.hub.name
   virtual_network_name = azurerm_virtual_network.hub.name
   address_prefixes     = [each.value]
@@ -39,8 +39,8 @@ resource "azurerm_subnet" "hub" {
 resource "azurerm_resource_group" "spoke" {
   for_each = local.spokes
 
-  name     = "rg-${each.key}-dev-01"
-  location = "North Europe"
+  name     = "rg-${each.key}-${var.environment}-01"
+  location = local.location
 }
 
 
@@ -48,7 +48,7 @@ resource "azurerm_resource_group" "spoke" {
 resource "azurerm_virtual_network" "spoke" {
   for_each            = local.spokes
 
-  name                = "vnet-${each.key}-dev-01"
+  name                = "vnet-${each.key}-${var.environment}-01"
   location            = azurerm_resource_group.spoke[each.key].location
   resource_group_name = azurerm_resource_group.spoke[each.key].name
   address_space       = [each.value.vnet]
@@ -58,7 +58,7 @@ resource "azurerm_virtual_network" "spoke" {
 resource "azurerm_subnet" "spoke" {
   for_each             = local.spokes
 
-  name                 = "snet-default-dev-01"
+  name                 = "snet-default-${var.environment}-01"
   resource_group_name  = azurerm_resource_group.spoke[each.key].name
   virtual_network_name = azurerm_virtual_network.spoke[each.key].name
   address_prefixes     = [cidrsubnet(each.value.vnet, 8, 1)]
